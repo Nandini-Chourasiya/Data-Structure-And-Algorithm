97e59893e18204654c1e70c68d3803ca6d7362c1
Nandini-Chourasiya <nandinichourasiya75@gmail.com>
2026-07-23 15:08:04 +0530

    Time: 2064 ms (8.15%), Space: 7.9 MB (79.64%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+    int solve( int n ){
+
+        if( n <= 1)
+        return 1;
+
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            int left = solve(i-1) ;
+            int right = solve(n-i);
+            ans += left*right;
+        }
+        return ans;
+    }
+
+    int numTrees(int n) {
+        return solve(n);
+    }
+};
